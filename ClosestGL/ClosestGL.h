ae809e01bc9735f2698a1594055792c80851c0fb
星翼 <853974536@qq.com>
2018-03-13 18:37:29 +0800

    Main Header.h

---
+++ ClosestGL/ClosestGL.h
@@ -0,0 +1,44 @@
+#pragma once
+
+//Math
+#include "CoordinateSystem.h"
+#include "MathCommon.h"
+#include "Matrix2.h"
+#include "Matrix3.h"
+#include "Matrix4.h"
+#include "MatrixTransform.h"
+#include "Number.h"
+#include "TypeMaxValues.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+#include "VectorCommon.h"
+
+//ParallelStrategy
+#include "MultiThreadRunner.h"
+#include "SingleThreadRunner.h"
+
+//Primitive
+#include "CVVClipper.h"
+#include "IndexBufferGenerator.h"
+#include "PerspectiveDivisionBuffer.h"
+#include "PrimitiveListReader.h"
+#include "PrimitiveReader.h"
+#include "PrimitiveStripReader.h"
+#include "Transformers.h"
+
+//RenderPipeline
+#include "DepthTest.h"
+#include "LineRasterizer.h"
+#include "PerspectiveCorrector.h"
+#include "PixelShader.h"
+#include "RenderTarget.h"
+#include "TriangleRasterizer.h"
+
+//Texture
+#include "TextureFliters.h"
+#include "Sampler2D.h"
+#include "UVNormalizer.h"
+
+#include "Texture2D.h"
+
